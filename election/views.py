8145b359bc1c3a9609863b507dfa5e16a3fb5245
Toshihiro <adigungodwin2@gmail.com>
2023-01-22 15:15:22 +0100

    renamed some variables

--- election/views.py
+++ election/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
-from election.models import AnnouncedPuResults
+from election.models import AnnouncedPuResults, PollingUnit
 
 # Create your views here.
 def question1(request):
-    polling_units = AnnouncedPuResults.objects.all()
-    return render(request, 'question1.html', {'polling_units': polling_units})
+    announced_pu_results = AnnouncedPuResults.objects.all()
+    return render(request, 'question1.html', {'announced_pu_results': announced_pu_results})
